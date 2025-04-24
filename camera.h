bb82e6de39a82c4fcec1a9bc6970e014612dea7f
lyra <n51g9cpm5t@hotmail.com>
2025-04-24 23:53:14 +0800

    continous

--- camera.h
+++ camera.h
@@ -5,8 +5,9 @@
 
 class camera {
   public:
-    double aspect_ratio = 1.0;  // Ratio of image width over height
-    int    image_width  = 100;  // Rendered image width in pixel count
+    double aspect_ratio = 1.0;
+    int    image_width  = 100;
+    int samples_per_pixel = 10;
 
     void render(const hittable& world) {
         initialize();
@@ -29,35 +30,47 @@ class camera {
     }
 
   private:
-    int    image_height;   // Rendered image height
-    point3 center;         // Camera center
-    point3 pixel00_loc;    // Location of pixel 0, 0
-    vec3   pixel_delta_u;  // Offset to pixel to the right
-    vec3   pixel_delta_v;  // Offset to pixel below
+    int    image_height;
+    double pixel_sample_scale;
+    point3 center;   
+    point3 pixel00_loc;
+    vec3   pixel_delta_u;
+    vec3   pixel_delta_v;
 
     void initialize() {
         image_height = int(image_width / aspect_ratio);
         image_height = (image_height < 1) ? 1 : image_height;
 
+        pixel_sample_scale = 1 / samples_per_pixel;
+
         center = point3(0, 0, 0);
 
-        // Determine viewport dimensions.
         auto focal_length = 1.0;
         auto viewport_height = 2.0;
         auto viewport_width = viewport_height * (double(image_width)/image_height);
 
-        // Calculate the vectors across the horizontal and down the vertical viewport edges.
         auto viewport_u = vec3(viewport_width, 0, 0);
         auto viewport_v = vec3(0, -viewport_height, 0);
 
-        // Calculate the horizontal and vertical delta vectors from pixel to pixel.
         pixel_delta_u = viewport_u / image_width;
         pixel_delta_v = viewport_v / image_height;
 
-        // Calculate the location of the upper left pixel.
         auto viewport_upper_left =
             center - vec3(0, 0, focal_length) - viewport_u/2 - viewport_v/2;
         pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+    }
+
+    ray get_ray(int i, int j) {
+        auto offset = sample_square();
+        auto pixel_sample = pixel00_loc + (i + offset.x()) * pixel_delta_u + (j + offset.y()) * pixel_delta_v;
+
+        auto ray_origin = center;
+        auto ray_direction = pixel_sample - ray_origin;
+        return ray(ray_origin, ray_direction);
+    }
+
+    vec3 sample_square() {
+        return vec3(random_double() - 0.5, random_double() - 0.5, 0);
     }
 
     color ray_color(const ray& r, const hittable& world) const {
